18fe36100e4de7d202cb41eb97b2a7a9939f7a3c
kotipek <sepo10072003@mail.ru>
2026-07-23 04:14:39 +0500

    Добавлен файл с json'ами и запись

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	"strings"
 	"time"
 	"todocli/models"
+	"todocli/storage"
 )
 
 var tasks []models.Task
 
 func main() {
+
 	var actionType string
 	if len(os.Args) > 1 {
 		actionType = os.Args[1]
 		switch actionType {
 		case "Создать":
 			tasks = append(tasks, createTask())
-			fmt.Println("таска - ", tasks[0])
+			task := createTask()
+			fmt.Println("таска - ", task)
 		default:
 			fmt.Println("Вы не выбрали действие")
 		}
@@ -30,7 +33,6 @@ func main() {
 			actionType = text
 			actionType = strings.TrimSpace(text)
 			fmt.Println(actionType)
-
 			switch actionType {
 			case "help":
 				fmt.Println("Список действий:")
@@ -41,9 +43,9 @@ func main() {
 				fmt.Print("Введите действие - ")
 			case "Создать":
 				tasks = append(tasks, createTask())
-				fmt.Println("таска - ", tasks)
-			case "Вывести":
-				getTasks()
+				storage.WriteTasks(tasks)
+			// case "Вывести":
+			// 	getTasks()
 			default:
 				fmt.Println("Введите help для подсказки")
 			}
@@ -59,12 +61,12 @@ func createTask() models.Task {
 		Description: "gjrbnrnbgjr"}
 }
 
-func getTasks() {
-	for i := range tasks {
-		fmt.Println("Название - ", tasks[i].Name,
-			"Статус - ", tasks[i].Status,
-			"Дата - ", tasks[i].Date,
-			"Описание - ", tasks[i].Description,
-			"Номер задачи - ", i+1)
-	}
-}
+// func getTasks() {
+// 	for i := range tasks {
+// 		fmt.Println("Название - ", tasks[i].Name,
+// 			"Статус - ", tasks[i].Status,
+// 			"Дата - ", tasks[i].Date,
+// 			"Описание - ", tasks[i].Description,
+// 			"Номер задачи - ", i+1)
+// 	}
+// }
